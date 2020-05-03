6d310c5d3770920cf225b2b2049e4cc43497dde4
Håvard Gulldahl <havard.gulldahl@nrk.no>
2020-05-03 16:01:20 +0200

    Add config file

--- Odaklokke/boot.py
+++ Odaklokke/boot.py
@@ -7,9 +7,11 @@ import network, utime  # pylint: disable=import-error
 
 from networking import wifi_up, telnet_up, mdns_up
 from clock import ntp_sync
+from config import WIFI_PASSWORD, WIFI_SSID
 
 print("boot.py")
 
+wifi_up(WIFI_SSID, WIFI_PASSWORD)
 
 ntp_sync()
 
